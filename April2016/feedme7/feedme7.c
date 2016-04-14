a3d078d81438fff7564b992ea27a492a48454c5b
Jon Moroney <jmoroney@hawaii.edu>
2016-04-13 17:15:26 -1000

    Updated hashing

---
+++ April2016/feedme7/feedme7.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <openssl/sha.h>
+
+void main (int argc, char *argv[])
+{
+  char answer[7] = "po'oihe";
+  unsigned char hash[SHA_DIGEST_LENGTH]; //
+  //printf("SHA_DIGEST_LENGTH = %d\n", SHA_DIGEST_LENGTH);
+  SHA1(answer, sizeof(answer), hash);
+
+  if (argc<2)
+  {
+    printf("Program takes one argument!\n");
+    return;
+  }
+  if (argc>2)
+  {
+    printf("Program takes one argument!\n");
+    return;
+  }
+
+  int comp = (memcmp(argv[1], answer, sizeof(answer)));
+  if (comp > 0)
+  {
+    printf("Input value over target value\n");
+    return;
+  }
+  if (comp<0)
+  {
+    printf("Input value under target value\n");
+    return;
+  }
+  if (comp==0)
+  {
+    for(int i = 0; i<20;i++)
+    {
+      printf("%x", hash[i]);
+    }
+    return;
+  }
+}
